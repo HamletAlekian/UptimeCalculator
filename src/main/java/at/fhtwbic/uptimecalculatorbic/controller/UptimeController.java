6ef93c1a2384e64bca826b10c10b1c9b3b91db5c
Hamlet <hamlet.alekian@gmail.com>
2022-11-09 20:05:46 +0100

    commit a test which fails

--- src/main/java/at/fhtwbic/uptimecalculatorbic/controller/UptimeController.java
+++ src/main/java/at/fhtwbic/uptimecalculatorbic/controller/UptimeController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UptimeController {
     @GetMapping( "/uptime/minutes")
-    public double calculateuptime(@RequestParam double relative) {
+    public double calculateUptimeMinutes(@RequestParam double relative) {
         double minutesOfMonth = 60*24*30;
         double defineUptimeInPercent = relative / 100;
         return minutesOfMonth + defineUptimeInPercent;
     }
 
     @GetMapping("/uptime/hours")
-    public double calculateuptime(@RequestParam double relative) {
+    public double calculateUptimeHours(@RequestParam double relative) {
         double hoursOfMonth = 24*30;
         double defineUptimeInPercent = relative / 100;
         return hoursOfMonth + defineUptimeInPercent;
---
+++ src/test/java/at/fhtwbic/uptimecalculatorbic/controller/UptimeControllerTest.java
@@ -0,0 +1,24 @@
+package at.fhtwbic.uptimecalculatorbic.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UptimeControllerTest {
+
+    @Test
+    void calculateUptimeMinutes() {
+        // a - arrange - anordnen, vorbereiten, d.h. Instanz der Klasse anlegen die ich testen will
+        var controller = new UptimeController();
+        // a - act = Test ausführen
+        var result = controller.calculateUptimeMinutes(0.0);
+        // a - assert - behaupten, begründen (Vergleichen, ob das Ergebnis der Erwartung entspricht)
+        assertEquals(0.0, result);
+
+    }
+
+    @Test
+    void calculateUptimeHours() {
+       assertEquals(1, 0);
+    }
+}
